51d2bac1d0ee47a35321ee7a73ec8cfe596226e1
Arthur d'Azémar <arthur.d-azemar@inria.fr>
2020-01-28 10:59:35 +0100

    bootstrap

--- src/main/java/fr/lille/univ/car/tp1/Main.java
+++ src/main/java/fr/lille/univ/car/tp1/Main.java
@@ -1,3 +1,22 @@
 package fr.lille.univ.car.tp1;
 
+class State{
 
+}
+
+class Answer{
+	public String requestAnswer;
+	public State state;
+
+	public Answer(String requestAnswer, State state) {
+		this.requestAnswer = requestAnswer;
+		this.state = state;
+	}
+}
+
+class CommandProcessor{
+
+	public Answer process(String request, State state) {
+		return null;
+	}
+}
